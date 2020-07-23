d7ab8b5268073d752cff72d418c61d1fefbb3a5f
MaciejIzdebski <izdebski.maciek@gmail.com>
2020-07-23 19:28:04 +0200

    Added NoteEditor(.css & .js), Note(.css & .js) files and icons. Changed index(.css & .html & .js)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Note from './Note';
 import * as serviceWorker from './serviceWorker';
+import noteEditor from 'NoteEditor'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+function AddNewNoteButton(props) {
+    return (
+        <button id="add-new-note" onClick={() => noteEditor.setVisablilty()}>
+            <img src={"icons/005-plus.svg"} alt="Dodaj notatke..."/>
+        </button>
+    )
+}
+
+var notes = ReactDOM.render(
+    <React.StrictMode>
+        <Note title="Tytuł" content="Testowa zawartość notatki" />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
